85655fb5be5c01779b1d7c9fc720cc0f2e31e483
Jps709 <jps7095@psu.edu>
2023-04-06 10:30:11 -0400

    Updoot 0.6.6

--- api/badge-data.js
+++ api/badge-data.js
@@ -10,6 +10,46 @@ export default async function handler(request, res) {
         "creatorIcon": "Test2",
         "badgeCreator": "Joshua Hantman",
         "compTime": "1.0 hour"
+    },
+    {   "badgeName": "Badge Name",
+    "badgeIcon": new URL('../assets/Cognito.png', import.meta.url).href,
+    "badgeDescription": "Learn the basics of how Amazon Cognito works, and how you can use it to create User Sign In, Sign In, Access Control, User Pools, and Identity Pools",
+    "descriptionLink": "Test1",
+    "linkName": "Cognito Info Link",
+    "breakLine": "------------------------------",
+    "creatorIcon": "Test2",
+    "badgeCreator": "Joshua Hantman",
+    "compTime": "1.0 hour"
+    },
+    {   "badgeName": "Badge Name",
+    "badgeIcon": new URL('../assets/Cognito.png', import.meta.url).href,
+    "badgeDescription": "Learn the basics of how Amazon Cognito works, and how you can use it to create User Sign In, Sign In, Access Control, User Pools, and Identity Pools",
+    "descriptionLink": "Test1",
+    "linkName": "Cognito Info Link",
+    "breakLine": "------------------------------",
+    "creatorIcon": "Test2",
+    "badgeCreator": "Joshua Hantman",
+    "compTime": "1.0 hour"
+    },
+    {   "badgeName": "Badge Name",
+    "badgeIcon": new URL('../assets/Cognito.png', import.meta.url).href,
+    "badgeDescription": "Learn the basics of how Amazon Cognito works, and how you can use it to create User Sign In, Sign In, Access Control, User Pools, and Identity Pools",
+    "descriptionLink": "Test1",
+    "linkName": "Cognito Info Link",
+    "breakLine": "------------------------------",
+    "creatorIcon": "Test2",
+    "badgeCreator": "Joshua Hantman",
+    "compTime": "1.0 hour"
+    },
+    {   "badgeName": "Badge Name",
+    "badgeIcon": new URL('../assets/Cognito.png', import.meta.url).href,
+    "badgeDescription": "Learn the basics of how Amazon Cognito works, and how you can use it to create User Sign In, Sign In, Access Control, User Pools, and Identity Pools",
+    "descriptionLink": "Test1",
+    "linkName": "Cognito Info Link",
+    "breakLine": "------------------------------",
+    "creatorIcon": "Test2",
+    "badgeCreator": "Joshua Hantman",
+    "compTime": "1.0 hour"
     }
     ];
 res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');

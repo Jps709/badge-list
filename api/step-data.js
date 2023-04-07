81472b8538d7e0e06d10cccf94aa305be5cf3dd2
Jps709 <jps7095@psu.edu>
2023-04-07 14:54:33 -0400

    Updoot 0.7.6

---
+++ api/step-data.js
@@ -0,0 +1,29 @@
+// export default async function handler(request, res) {
+
+//     const badgeData = [
+//         {
+//             "tag": "Amazon Cognito",
+//             "stepIcon": 'av:play-circle-outline',
+//             "stepDescription": "Create a User Pool in Amazon Cognito",
+//             "stepTime": "4.0 hours"
+//         },
+//         {
+//             "tag": "AWS- LAMBDA Phase 1",
+//             "stepIcon": 'av:play-circle-outline',
+//             "stepDescription": "Amazon Incognito",
+//             "stepTime": "4.0 hours"
+//         },
+//         {
+//             "tag": "AWS- LAMBDA Phase 1",
+//             "stepIcon": 'build',
+//             "stepDescription": "Taco Tyesday",
+//             "stepTime": "4.0 hours"
+//         }
+//     ];
+//     res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+//     res.json(badgeData);
+// }
